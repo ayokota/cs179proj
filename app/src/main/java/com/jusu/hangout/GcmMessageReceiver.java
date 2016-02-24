a7adb96e7221ebbd920a6eab1381a64a4048a0ea
akiyo yokota <ayoko001@akiyos-MBP.attlocal.net>
2016-02-23 23:53:40 -0800

    made main activity link to app start

--- app/src/main/java/com/jusu/hangout/GcmMessageReceiver.java
+++ app/src/main/java/com/jusu/hangout/GcmMessageReceiver.java
@@ -70,7 +70,7 @@ public class GcmMessageReceiver extends GcmListenerService {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.ic_stat_ic_notification)
-                .setContentTitle("GCM Message")
+                .setContentTitle("Hangout Message")
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
